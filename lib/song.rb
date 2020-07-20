5b8aef5783b56a28c4a69ba4c1a3612a3cf65e1d
Tiauna Paul <tiaunaj.tsg@gmail.com>
2020-07-20 04:35:14 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -72,67 +72,11 @@ class Song
 
 end
 
-
-
-#this is for the create_from_filename method & destroy_all
-#song = Song.create_from_filename("Taylor Swift - Blank Space.mp3")
-#song = Song.create_from_filename("Test 02 - Test_02.mp3")
-#song = Song.create_from_filename("Test 03 - Test_03.mp3")
-#puts Song.all.inspect
-#Song.destroy_all
-#puts Song.all.inspect
-
-#song.name #=> "Blank Space"
-#song.artist_name #=> "Taylor Swift"
-
-
-#this is for new_from_filename method
-#song = Song.new_from_filename("Taylor Swift - Blank Space.mp3")
-#song.name #=> "Blank Space"
-#song.artist_name #=> "Taylor Swift"
-
-
-#this is for the alphabetical method
-#song = Song.create_by_name("Cranberry")
-#song = Song.create_by_name("Apples")
-#song = Song.create_by_name("Berry")
-#Song.alphabetical
-
-
-#this is for the find_or_create_by_name method
 song_1 = Song.find_or_create_by_name("Blank Space")
 song_2 = Song.find_or_create_by_name("Blank Space")
  
-# song_1 and song_2 are conceptually the same song and should return the same song instance because of `.find_or_create_by_name.`
- 
-#song_1 == song_2 #=> true
 
 
-
-#for find_by_name method
-#the_middle = Song.create_by_name("The Middle")
-#=> #<Song @name="The Middle">
-#Song.find_by_name("The Middle")
-#<Song @name="The Middle">
-
-#for create_by_name method
-#song = Song.create_by_name("The Middle")
-#=> #<Song:0x007fd2a2989ff0 @name="The Middle">
-#song
-#=> #<Song:0x007fd2a2989ff0 @name="The Middle">
-#Song.all.include?(song)
-#=> true
-
-#for create method
-#song01 = Song.create
-#song02 = Song.create
-#song03 = Song.create
-#Song.all.include?(song03) #=> true
-
-#for new_by_name method
-#song = Song.new_by_name("The Middle")
-#=> #<Song @name="The Middle">
-#song.name #=> "The Middle"
 
 
 
